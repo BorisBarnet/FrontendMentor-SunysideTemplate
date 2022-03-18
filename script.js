54cc8be294ad43cf96e554df8d9a1fdf9230b75c
Hadrien AICARDO <seditieux.kazakh@gmail.com>
2022-03-18 05:41:11 +0100

    First commit

---
+++ script.js
@@ -0,0 +1,11 @@
+
+function showMenu() {
+const menuMobile = document.getElementById("menu-mobile")
+
+
+  menuMobile.classList.toggle("div-hided")
+  menuMobile.style.animationName = "animation"
+  menuMobile.style.animationDuration = "400ms"
+  menuMobile.style.animationFillMode = "forwards"
+
+}
